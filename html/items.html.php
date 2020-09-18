c3d815f9d0d4b8508e26f56539d0a4cfd325bd97
Nicole <43906055+Knotonium@users.noreply.github.com>
2020-09-18 09:57:49 +0200

    Update items.html.php

--- html/items.html.php
+++ html/items.html.php
@@ -6,7 +6,6 @@
         <th>Nr</th>
         <th>URL</th>
         <th>Notiz</th>
-        <th>Löschen</th>
     </tr>
     <?php foreach ($items as $item) :?>
     <tr>
@@ -14,7 +13,7 @@
         <td><?= htmlspecialchars($item->getId())?></td>
         <td><a href="<?= htmlspecialchars($item->getUrl())?>"><?= htmlspecialchars($item->getUrl())?></a></td>
         <td><?= htmlspecialchars($item->getCode())?></td>
-        <td><input type='button' name='deletebtn' value='x'></td><!-- <button name='deletebtn' value="< ?= htmlspecialchars($item->getId())?>">x</button>-->
+
     </tr>
     <?php endforeach;?>
 </table>

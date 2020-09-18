54930e9012fbe069e4eb6b0f6214322599396120
Nicole <43906055+Knotonium@users.noreply.github.com>
2020-09-18 09:56:31 +0200

    Update form.html.php

--- html/form.html.php
+++ html/form.html.php
@@ -2,4 +2,4 @@
     <input type='text' name='url' maxlength='1000' size='100' placeholder='Trage die Url ein'><br>
     <input type='text' name='code' maxlength='1000' size='100' placeholder='Trage eine Notiz dazu ein'><br>
     <input type='submit' name='shortbtn' value='make it so'>
-</form>
+</form> 
